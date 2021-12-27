68e61d7478e069b0c7b9d262668ec23ca1e9545b
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-12-27 18:11:26 +0800

    создан общий редусер

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import filtersReducers from './filters';
+import pizzasReducers from './pizzas';
+
+const rootReducers = combineReducers({
+  filters: filtersReducers,
+  pizzas: pizzasReducers,
+});
+
+export default rootReducers;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,11 @@
 import { createStore } from 'redux';
+import rootReducers from './reducers';
 
-function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 };
-    case 'counter/decremented':
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
+const store = createStore(
+  rootReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
-const store = createStore(counterReducer, 1);
+window.store = store;
 
 export default store;
