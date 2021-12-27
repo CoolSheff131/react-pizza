5681584aa69bbd5a456d4781ea7b3a50331f9d6d
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-12-27 17:24:46 +0800

    Подключен реакт редакс

--- src/index.js
+++ src/index.js
@@ -1,42 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore } from 'redux';
-
 import './scss/app.scss';
-
 import App from './App';
-
-function counterReducer(state = { value: 0 }, action) {
-  switch (action.type) {
-    case 'incremented':
-      return { value: state.value + 1 };
-    case 'decremented':
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
-
-// Create a Redux store holding the state of your app.
-// Its API is { subscribe, dispatch, getState }.
-const store = createStore(counterReducer);
-
-store.subscribe(() => console.log('aaa', store.getState()));
-
-// The only way to mutate the internal state is to dispatch an action.
-// The actions can be serialized, logged or stored and later replayed.
-store.dispatch({ type: 'incremented' });
-// {value: 1}
-store.dispatch({ type: 'incremented' });
-// {value: 2}
-store.dispatch({ type: 'decremented' });
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
+  <Router>
+    <Provider store={store}>
       <App />
-    </Router>
-  </React.StrictMode>,
+    </Provider>
+  </Router>,
   document.getElementById('root'),
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Categories, PizzaBlock, SortPopup } from '../components';
+import store from '../redux/store';
+
+
+const inc = () => {
+  store.dispatch({
+    type: 'counter/incremented',
+  });
+};
+
+store.subscribe(() => {
+  console.log('AAS', store.getState());
+});
+
 
 function Home({items}) {
 
     return (
         <div className="container">
+          <button onClick={inc}></button>
           <div className="content__top">
             <Categories
               onClickItem={(name) => alert(name)}
               items={['Мясные', 'Вегетарианские', 'Гриль', 'Острые', 'Закрытые']}
             />
-            <SortPopup items={['популярности', 'цене', 'алфавиту']} />
+            <SortPopup items={[{name: 'популярности', type: 'popular'},{name: 'цене',type: 'price'}, {name: 'алфавиту',type: 'alphabet'}]} />
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
