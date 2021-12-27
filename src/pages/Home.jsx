085fade94779bef64cf524f6f90acc7ff5893ded
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-12-27 16:46:57 +0800

    протестирован редакс

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home({items}) {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {items.map((obj) => {
-              return <PizzaBlock key={obj.id} {...obj} name='Test'/>
+              return <PizzaBlock key={obj.id} {...obj}/>
             })}            
           </div>
         </div>
