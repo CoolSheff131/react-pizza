486b7be0d9da8e4cbe45cb969691f93cb80fdce4
Александр Никульшеев <a.nikulsheev@gmail.com>
2021-12-30 20:59:14 +0800

    Добавлены ключи скелетонам

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ function Home() {
   const dispatch = useDispatch();
   const items = useSelector(({ pizzas }) => pizzas.items);
   const isLoaded = useSelector(({ pizzas }) => pizzas.isLoaded);
+  const {category, sortBy } = useSelector(({ filters }) => filters);
 
   const onSelectCategory = React.useCallback(index => {
     dispatch(setCategory(index))
@@ -19,7 +20,7 @@ function Home() {
 
   React.useEffect(() => {
     dispatch(fetchPizzas());
-  }, [dispatch]);
+  }, [dispatch, ]);
 
   return (
         <div className="container">
@@ -34,7 +35,7 @@ function Home() {
           <div className="content__items">
             {isLoaded ? items.map((obj) => {
               return <PizzaBlock key={obj.id} {...obj}/>
-            }): Array(10).fill(<PizzaLoadingBlock/>)}        
+            }): Array(10).fill(0).map((_,index)=> <PizzaLoadingBlock key={index}/>)}
           </div>
         </div>
     )
